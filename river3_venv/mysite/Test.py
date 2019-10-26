672e997e5c537692320d5773b24e4193b3636a44
unknown <z331000@gmail.com>
2019-10-26 15:14:44 +0800

    Inspect Rule

--- river3_venv/mysite/Test.py
+++ river3_venv/mysite/Test.py
@@ -3,10 +3,12 @@
 
 import sqlite3
 import pandas as pd
+import InspectRule as IR
 conn = sqlite3.connect('db.sqlite3')
 #cursor = conn.execute("riverName")
 df=pd.read_sql_query("select * from OceanInspect_oceaninspectmodel ", conn)
 df_export=pd.read_sql_query("select * from OceanInspect_oceaninspectexport",conn)
+"""
 print("\n BEFORE \n" ,df)
 
 length_table=len(df) #探測table長度
@@ -21,3 +23,5 @@ while i<length_table:   #定義迴圈終點
 df_export.to_sql('OceanInspect_oceaninspectexport',con=conn,if_exists='replace',index=False)
 #print ("\n ==================== \n" , df_export) #將寫好的dataframe寫回sqlite的 database
 conn.close() #關閉連結
+"""
+print("\n IR_point == ",IR.RPI(5,10,10,10),"==\n")
