03bf3124ff8675e7524e84f3d8d6e2f82528cb75
yijung <chenyijung.1996@gmail.com>
2019-10-27 00:03:32 +0800

    WQI Rule Finish

--- river3_venv/mysite/InspectRule.py
+++ river3_venv/mysite/InspectRule.py
@@ -124,3 +124,99 @@ def RPIInspect (point):
     if point>6:
         point_Result=('嚴重汙染')
     return point_Result
+
+#====================WQI_Rule===========================
+
+def WQI_PH(PH):
+    PH_point=0
+    if PH>=6.5 and PH<=8.5:
+        PH_point=1
+    else:
+        PH_point=2
+    return PH_point
+
+def WQI_O2(O2):
+    O2_point=0
+    if O2=="ND":
+        O2_point=6
+    if O2>=6.5:
+        O2_point=1
+    if O2>=5.5:
+        O2_point=2
+    if O2>=4.5:
+        O2_point=3
+    if O2>=3:
+        O2_point=4
+    if O2<3:
+        O2_point=5
+    return O2_point
+
+def WQI_BOD(BOD):
+    BOD_point=0
+    if BOD=="ND":
+        BOD_point=1
+    if BOD<=1:
+        BOD_point=1
+    if BOD<=2:
+        BOD_point=2
+    if BOD<=4:
+        BOD_point=3
+    if BOD>4:
+        BOD_point=4
+    return BOD_point
+
+def WQI_SS(SS):
+    SS_point=0
+    if SS=="ND":
+        SS_point=1
+    if SS<=25:
+        SS_point=1
+    if SS<=40:
+        SS_point=3
+    if SS<=100:
+        SS_point=4
+    if SS>100:
+        SS_point=5
+    return SS_point
+
+def WQI_CFU(CFU):
+    CFU_point=0
+    if CFU=="ND":
+        CFU_point=1
+    if CFU<=50:
+        CFU_point=1
+    if CFU<=5000:
+        CFU_point=2
+    if CFU<=10000:
+        CFU_point=3
+    if CFU>10000:
+        CFU_point=4
+    return CFU_point
+
+def WQI_N2(N2):
+    N2_point=0
+    if N2=="ND":
+        N2_point=1
+    if N2<=0.1:
+        N2_point=1
+    if N2<=0.3:
+        N2_point=2
+    if N2>0.3:
+        N2_point=4
+    return N2_point
+
+def WQI_TP(TP):
+    TP_point=0
+    if TP=="ND":
+        TP_point=1
+    if TP<=0.02:
+        TP_point=1
+    if TP<=0.05:
+        TP_point=2
+    if TP>0.05:
+        TP_point=3
+    return TP_point
+
+def WQI_level(WQI_PH,WQI_O2,WQI_BOD,WQI_SS,WQI_CFU,WQI_N2,WQI_TP):
+    maxpoint=max(WQI_PH,WQI_O2,WQI_BOD,WQI_SS,WQI_CFU,WQI_N2,WQI_TP)
+    return maxpoint
