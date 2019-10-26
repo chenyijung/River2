4672c92066ae8dbb715e759c96e4d2948b983a2c
yijung <chenyijung.1996@gmail.com>
2019-10-26 14:50:19 +0800

    Test.py Test

--- river3_venv/mysite/Test.py
+++ river3_venv/mysite/Test.py
@@ -5,13 +5,36 @@ import sqlite3
 import pandas as pd
 conn = sqlite3.connect('db.sqlite3')
 #cursor = conn.execute("riverName")
-df=pd.read_sql_query("select * from RiverStationBasic_riverstation ", conn)
-print("\n BEFORE \n" ,df)
+df=pd.read_sql_query("select * from RiverInspect_riverinspectmodel ", conn)
+#print("\n BEFORE \n" ,df)
 
-i=0
+        
+#print(df.index.stop)
+#for i in range(int(df.index)):
+#    print(i)
 
-while i!=1:
-    print(df.loc[i,"TestData1"],"\n")
-    i=i+1
+test_filter = df.loc [7,'inspectO2']
+
+point = 0
+
+if (test_filter<7):
+    print("小於7")
+    point=point+1
+else:
+    print("大於7")
+    point=point+2
+#df[ df['riverName']=='旱溪'] ]
+
+print(test_filter)
+print("點數為",point)
+
+
+
+#i=0
+
+#while i!=1:
+#    print(df.loc[i,"TestData1"],"\n")
+#    i=i+1
 
 #print ("Name = " , row[0])
+
